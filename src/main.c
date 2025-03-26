a4ddd0ca4cf49a92d841c930b31b04b6772cd90a
Kevin <jekevin964@gmail.com>
2025-03-26 15:26:45 -0400

    hello rectangle

--- src/main.c
+++ src/main.c
@@ -26,23 +26,35 @@ int main()
         float vertices[] = {
                 -0.5f, -0.5f, 0.0f,
                  0.5f, -0.5f, 0.0f,
-                 0.0f,  0.5f, 0.0f
+                 0.5f,  0.5f, 0.0f,
+                -0.5f,  0.5f, 0.0f
         };
+
+        int indices[] = {
+                0, 1, 3,
+                1, 2, 3
+        };
+
         // clang-format on
 
         unsigned int vao;
         unsigned int vbo;
-        // unsigned int ebo;
+        unsigned int ebo;
 
         glGenVertexArrays(1, &vao);
         glGenBuffers(1, &vbo);
-        // glGenBuffers(1, &ebo);
+        glGenBuffers(1, &ebo);
 
         glBindVertexArray(vao);
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
                      GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                     GL_STATIC_DRAW);
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                               (void *)0);
         glEnableVertexAttribArray(0);
@@ -51,12 +63,12 @@ int main()
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
         while (!glfwWindowShouldClose(window.self)) {
-                glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+                glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
 
                 glUseProgram(shader);
                 glBindVertexArray(vao);
-                glDrawArrays(GL_TRIANGLES, 0, 3);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
                 handle_input(window.self);
                 glfwSwapBuffers(window.self);
