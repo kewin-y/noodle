6af6ce0a15ddad98452a2dd4f098df2318a21caf
Kevin <jekevin964@gmail.com>
2026-01-29 19:51:25 -0500

    use cmake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,69 @@
 {
-  description = "Can't wait to";
+  description = "Minecraft Clone";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bear
-            valgrind-light
-            clang-tools
-          ];
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-linux"
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  in {
+    formatter = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        pkgs.alejandra
+    );
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = builtins.attrValues {
+            inherit
+              (pkgs)
+              valgrind-light
+              clang-tools
+              cmake
+              pkg-config
+              wayland
+              libffi
+              libxkbcommon
+              wayland-scanner
+              git
+              ;
 
-          buildInputs = with pkgs; [
-            glfw
-            cglm
-            stb
-            xorg.libX11
-            xorg.libXrandr
-            xorg.libXi
-          ];
+            inherit
+              (pkgs.xorg)
+              libX11
+              libXrandr
+              libXi
+              libXinerama
+              libXcursor
+              ;
+          };
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (builtins.attrValues {
+            inherit
+              (pkgs)
+              wayland
+              libxkbcommon
+              libGL
+              libglvnd
+              mesa
+              ;
+          });
+
+          shellHook = ''
+            export SHELL='${pkgs.mksh}/bin/mksh'
+          '';
         };
-      };
-    };
+      }
+    );
+  };
 }
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "window.h"
 #include <GLFW/glfw3.h>
 #include <cglm/cglm.h>
+#include <config.h>
 #include <math.h>
 
 // GLOBALS
@@ -21,228 +22,225 @@ float last_frame = 0.0f;
 
 static void handle_input(GLFWwindow *win)
 {
-        const float camera_speed = 2.5f * delta_time;
+    const float camera_speed = 2.5f * delta_time;
 
-        vec3 right, forward;
+    vec3 right, forward;
 
-        if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-                glfwSetWindowShouldClose(win, 1);
-        }
+    if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(win, 1);
+    }
 
-        glm_vec3_cross(camera_front, camera_up, right);
-        glm_vec3_normalize(right);
-        glm_vec3_cross(camera_up, right, forward);
-        glm_vec3_normalize(forward);
+    glm_vec3_cross(camera_front, camera_up, right);
+    glm_vec3_normalize(right);
+    glm_vec3_cross(camera_up, right, forward);
+    glm_vec3_normalize(forward);
 
-        if (glfwGetKey(win, GLFW_KEY_W) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(forward, camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_W) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(forward, camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 
-        if (glfwGetKey(win, GLFW_KEY_S) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(forward, -camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_S) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(forward, -camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 
-        if (glfwGetKey(win, GLFW_KEY_A) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(right, -camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_A) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(right, -camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 
-        if (glfwGetKey(win, GLFW_KEY_D) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(right, camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_D) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(right, camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 
-        if (glfwGetKey(win, GLFW_KEY_SPACE) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(camera_up, camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(camera_up, camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 
-        if (glfwGetKey(win, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-                vec3 tmp;
-                glm_vec3_scale(camera_up, -camera_speed, tmp);
-                glm_vec3_add(camera_pos, tmp, camera_pos);
-        }
+    if (glfwGetKey(win, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        vec3 tmp;
+        glm_vec3_scale(camera_up, -camera_speed, tmp);
+        glm_vec3_add(camera_pos, tmp, camera_pos);
+    }
 }
 
 static void mouse_callback(GLFWwindow *win, double x_pos, double y_pos)
 {
-        if (first_mouse) {
-                last_x = x_pos;
-                last_y = y_pos;
-                first_mouse = 0;
-        }
-
-        float x_offset = x_pos - last_x;
-        float y_offset = last_y - y_pos; // As (0,0) is in the top-left corner
-        const float sensitivity = 0.2f;
-
-        vec3 direction;
-
+    if (first_mouse) {
         last_x = x_pos;
         last_y = y_pos;
+        first_mouse = 0;
+    }
 
-        x_offset *= sensitivity;
-        y_offset *= sensitivity;
+    float x_offset = x_pos - last_x;
+    float y_offset = last_y - y_pos; // As (0,0) is in the top-left corner
+    const float sensitivity = 0.2f;
 
-        yaw += x_offset;
-        pitch += y_offset;
+    vec3 direction;
 
-        if (pitch > 89.0f)
-                pitch = 89.0f;
-        if (pitch < -89.0f)
-                pitch = -89.0f;
+    last_x = x_pos;
+    last_y = y_pos;
 
-        direction[0] = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
-        direction[1] = sin(glm_rad(pitch));
-        direction[2] = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+    x_offset *= sensitivity;
+    y_offset *= sensitivity;
 
-        glm_normalize_to(direction, camera_front);
+    yaw += x_offset;
+    pitch += y_offset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    direction[0] = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
+    direction[1] = sin(glm_rad(pitch));
+    direction[2] = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+
+    glm_normalize_to(direction, camera_front);
 }
 
 int main()
 {
-        struct Window win;
-        struct Texture texture;
+    struct Window win;
+    struct Texture texture;
 
-        unsigned int shader;
+    unsigned int shader;
 
-        unsigned int vao, vbo;
+    unsigned int vao, vbo;
 
-        mat4 model, view, projection;
+    mat4 model, view, projection;
 
-        float current_frame;
+    float current_frame;
 
-        n_init_window(&win, 800, 600, "noodle");
+    n_init_window(&win, 800, 600, "noodle");
 
-        // AWFUL
-        glfwSetInputMode(win.self, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        glfwSetCursorPosCallback(win.self, mouse_callback);
+    glfwSetInputMode(win.self, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(win.self, mouse_callback);
 
-        n_init_texture(&texture, "assets/textures/dirt.png");
-        shader = n_create_shader("assets/shaders/simpleVert.glsl",
-                                 "assets/shaders/simpleFrag.glsl");
+    n_init_texture(&texture, ASSETS_PATH "textures/dirt.png");
 
-        // clang-format off
-        float vertices[] = {
-                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-                 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-                -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    shader = n_create_shader(ASSETS_PATH "shaders/simpleVert.glsl",
+                             ASSETS_PATH "shaders/simpleFrag.glsl");
 
-                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-                 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-                -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    // clang-format off
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-                -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-                -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-                 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-                 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-                 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-                 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-                -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-                -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-                 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-                 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-                -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-                -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-        };
-        // clang-format on
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-        glGenVertexArrays(1, &vao);
-        glGenBuffers(1, &vbo);
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    // clang-format on
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+
+    // Setup VBO
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                 GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // a_tex_coord
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                          (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glUseProgram(shader);
+    n_shader_set_uniform_i(shader, "v_texture", 0);
+
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(win.self)) {
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        current_frame = glfwGetTime();
+        delta_time = current_frame - last_frame;
+        last_frame = current_frame;
+
+        // printf("delta_time=%f\n", delta_time);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture.id);
+
+        glUseProgram(shader);
+
+        glm_vec3_print(camera_front, stdout);
+        glm_vec3_add(camera_pos, camera_front, camera_center);
+        glm_lookat(camera_pos, camera_center, camera_up, view);
+
+        glm_perspective(glm_rad(45.0f), (float)win.width / (float)win.height, 0.1f, 100.0f, projection);
+
+        n_shader_set_uniform_m4(shader, "view", &view[0][0]);
+        n_shader_set_uniform_m4(shader, "projection", &projection[0][0]);
 
         glBindVertexArray(vao);
 
-        // Setup VBO
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
-                     GL_STATIC_DRAW);
+        /* AGHH */
+        glm_mat4_identity(model);
+        n_shader_set_uniform_m4(shader, "model", &model[0][0]);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-                              (void *)0);
-        glEnableVertexAttribArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // a_tex_coord
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
-                              (void *)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
+        handle_input(win.self);
+        glfwSwapBuffers(win.self);
+        glfwWaitEvents();
+    }
 
-        glBindVertexArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-        glUseProgram(shader);
-        n_shader_set_uniform_i(shader, "v_texture", 0);
-
-        glEnable(GL_DEPTH_TEST);
-
-        while (!glfwWindowShouldClose(win.self)) {
-                glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-                current_frame = glfwGetTime();
-                delta_time = current_frame - last_frame;
-                last_frame = current_frame;
-
-                // printf("delta_time=%f\n", delta_time);
-
-                glActiveTexture(GL_TEXTURE0);
-                glBindTexture(GL_TEXTURE_2D, texture.id);
-
-                glUseProgram(shader);
-
-                glm_vec3_print(camera_front, stdout);
-                glm_vec3_add(camera_pos, camera_front, camera_center);
-                glm_lookat(camera_pos, camera_center, camera_up, view);
-
-                glm_perspective(glm_rad(45.0f),
-                                (float)win.width / (float)win.height,
-                                0.1f, 100.0f, projection);
-
-                n_shader_set_uniform_m4(shader, "view", &view[0][0]);
-                n_shader_set_uniform_m4(shader, "projection",
-                                        &projection[0][0]);
-
-                glBindVertexArray(vao);
-
-                /* AGHH */
-                glm_mat4_identity(model);
-                n_shader_set_uniform_m4(shader, "model", &model[0][0]);
-
-                glDrawArrays(GL_TRIANGLES, 0, 36);
-
-                handle_input(win.self);
-                glfwSwapBuffers(win.self);
-                glfwWaitEvents();
-        }
-
-        glfwTerminate();
-        return 0;
+    glfwTerminate();
+    return 0;
 }
